c2b8ee4cc2331474f40c69b5b9ded9f8f316a7fe
kpatel115 <kp9338448@gmail.com>
2021-07-23 12:09:17 -0600

    cancelled Log -added dynamic - JS to HTML function

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Global variables
+const itemList = document.getElementById("item-list");
+itemList.innerHTML = "<li>Hello World</li>"
+
+const itemQuantity = document.getElementById("item-quantity");
+itemQuantity.innerHTML = "<h3>Quantity</h3>"
+
+const itemTotal = document.getElementById("item-total");
+itemTotal.innerHTML = "<h4>Total</h4>"
+
 const cart = []
 
 //-----------------ADD ITEMS-------------------------------
@@ -10,7 +19,6 @@ function addItem(name, price) {
       return
     }
   }
-
   const item = {name, price, qty: 1}
   cart.push(item);
 
@@ -21,22 +29,28 @@ function showItem() {
   const qty = getQuantity();
   const total = getTotal();
 
-  console.log( `You have ${qty} items in your cart!` );
+  let itemString = "" // list of ul
+  
   // listing items with price and quantity
   for (let i = 0; i < cart.length; i += 1) {
-    console.log( `${cart[i].name} ${cart[i].price} x ${cart[i].qty}` )
+    //console.log( `${cart[i].name} ${cart[i].price} x ${cart[i].qty}` )
+    itemString += `<li>${cart[i].name} ${cart[i].price} x ${cart[i].qty}</li>`
   }
-  console.log ( `Total in cart: $${total}` );
+  itemList.innerHTML = itemString
+  // set to DOM with inner html
 };
 
 //---------------GET QUANTITY--------------------------
 function getQuantity() {
-  // total quantity amount 
-  let qty = 0;
 
+  let qty = 0;
+  let itemQty = "" // H3 tag console.log( `You have ${qty} items in your cart!` );
+  
   for (let p = 0; p < cart.length; p+= 1) {
     qty += cart[p].qty
   }
+  itemQty += `<h3>You have ${qty} items in your cart!</h3>`
+  itemQuantity.innerHTML = itemQty
   return qty
 };
 
@@ -44,19 +58,50 @@ function getQuantity() {
 function getTotal() {
   // total price amount 
   let total = 0;
+  let itemTot = "" // H4 tag console.log ( `Total in cart: $${total}` );
   for (let i = 0; i < cart.length; i += 1) {
     total += cart[i].price * cart[i].qty
   }
+  itemTot += `<h4>Total in cart: $${total.toFixed(2)}</h4>`
+  itemTotal.innerHTML = itemTot
   // toFixed(number of decimals) returns a string
   // do arithmetic first then use this to display
   return total.toFixed(2)
 };
 
+//----------------Remove Item----------------------------
+function removeItem(name, qty = 0){
+
+  for (let i = 0; i < cart.length; i += 1) {
+
+    if (cart[i].name === name) {
+
+      if (qty > 0) {
+        cart[i].qty -= qty
+      }
+
+      if (cart[i].qty < 1 || qty === 0) {
+        cart.splice(i, 1)
+      } 
+      return
+    }
+  }
+};
+
+
 //---------------FUNCTION CALLS----------------------------------
 // Function calls  w/ parameters
-addItem("Apple", 0.49);
-addItem("Orange", 0.99);
-addItem("Banana", 1.25);
-addItem("Apple", 0.49);
+addItem("Apple", 0.99);
+addItem("Orange", 1.29);
+addItem("Opinion", 0.02);
+addItem("Apple", 0.99);
+addItem("Frisbee", 9.92);
+addItem("Apple", 0.99);
+addItem("Orange", 1.29);
+
+showItem(); 
+
+removeItem("Apple", 1)
+removeItem("Frisbee")
 
 showItem(); 
