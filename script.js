dc38f948874b3da2cbf2c01b29643c139981a54c
kpatel115 <kp9338448@gmail.com>
2021-07-22 18:59:09 -0600

    HTML, CSS and JS files updates

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const = [];
+const cart = []
 
-function addItems() {
-
+function addItem(name, price) {
+  const item = {name: name, price: price, qty: 1}
+  cart.push(item);
 }
 
 function showItem() {
-
+  console.log(`You have ${cart.length} items in you cart!`);
 }
+
+addItem("Apple", 0.49);
+addItem("Orange", 0.99);
+addItem("Banana", 1.25);
+
+showItem(); 
